ea9a63e838762af14a3c24469792f5875463d194
Daniel Birch <daniel@danielbirch.dev>
2023-04-07 03:42:18 +0300

    Add local in development project files to GitHub.

---
+++ src/app.js
@@ -0,0 +1,4 @@
+// Generate portfolio cards based on the number of portfolio files in the /src/projects folder
+// Use the FileReader API: https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+
+// Dynamically create and insert icons <li> for project stack based upon the project file
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Pacaembu, sans-serif'],
+      'body': ['Mulish, sans-serif'],
+    }
+  },
+  plugins: [],
+}
+
