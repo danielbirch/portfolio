9a578c87de51b2bcaa6005ccbd29da1c16a6ca32
Daniel Birch <daniel@danielbirch.dev>
2023-04-08 02:35:51 +0300

    Add logo, written content, styling. Layout complete.

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
+// DARK MODE TOGGLE
+// Reference the IDs and store in a variable
+const dark = document.getElementById('dark');
+const light = document.getElementById('light');
+const html = document.querySelector('html');
+// Initialise event listener
+dark.addEventListener('click', toggleDarkMode);
+light.addEventListener('click', toggleDarkMode);
+// Toggle class 'hidden' on icons and class 'dark' on HTML tag
+function toggleDarkMode() {
+  if (html.classList.contains('dark')) {
+    dark.classList.toggle('hidden');
+    light.classList.toggle('hidden');
+    html.classList.remove('dark');
+  } else {
+    light.classList.toggle('hidden');
+    dark.classList.toggle('hidden');
+    html.classList.add('dark');
+  }
+}
+// Store light or dark mode in local storage
+
 // Generate portfolio cards based on the number of portfolio files in the /src/projects folder
 // Use the FileReader API: https://developer.mozilla.org/en-US/docs/Web/API/FileReader
 
 // Dynamically create and insert icons <li> for project stack based upon the project file
+
