6e054491e0becffda4d5f07fb2e024572ab7a369
Daniel Birch <daniel@danielbirch.dev>
2023-05-01 01:35:02 +0300

    Complete mobile, tablet and desktop styling along with dark mode. Fix issue with footer dynamic date not working.

--- app.js
+++ app.js
@@ -1,49 +1,60 @@
 // GLOBAL SCOPE
 
+// MOBILE MENU ACTION
+const mobileMenu = document.getElementById('mobile-icon');
+mobileMenu.addEventListener('click', openMenu);
 
-// DARK MODE TOGGLE
-// Reference the IDs and store in a variable
-const dark = document.getElementById('dark');
-const light = document.getElementById('light');
-const html = document.querySelector('html');
-// Initialise event listener
-dark.addEventListener('click', toggleDarkMode);
-light.addEventListener('click', toggleDarkMode);
+function openMenu() {
+  const menuToHide = document.getElementById('mobile-menu');
+  menuToHide.classList.toggle('hidden');
+}
+
+// DARK MODE TOGGLE V2
+const toggleElements = document.querySelectorAll('.toggle-dark-mode');
+const htmlUpdate = document.querySelector('html');
+
+toggleElements.forEach(element => {
+  element.addEventListener('click', toggleDarkMode);
+});
+
 document.addEventListener('DOMContentLoaded', loadMode);
-// Toggle class 'hidden' on icons and class 'dark' on HTML tag
+
 function toggleDarkMode() {
-  if (html.classList.contains('dark')) {
-    dark.classList.toggle('hidden');
-    light.classList.toggle('hidden');
-    html.classList.remove('dark');
+  if (htmlUpdate.classList.contains('dark')) {
+    toggleElements.forEach(element => {
+      element.classList.toggle('hidden');
+      htmlUpdate.classList.remove('dark');
+    })
   } else {
-    light.classList.toggle('hidden');
-    dark.classList.toggle('hidden');
-    html.classList.add('dark');
+    toggleElements.forEach(element => {
+      element.classList.toggle('hidden');
+      htmlUpdate.classList.add('dark');
+    })
   }
   saveMode();
 }
+
 // Save dark mode preference in local storage
 function saveMode() {
-  var saveValue = html.classList.contains('dark');
+  var saveValue = htmlUpdate.classList.contains('dark');
   localStorage.setItem('Dark Mode', saveValue);
 }
 // Load dark mode preference on page load
 function loadMode(saveValue) {
   const getValue = localStorage.getItem('Dark Mode');
   if (getValue === 'true') {
-    html.classList.add('dark');
+    htmlUpdate.classList.add('dark');
       if (dark.classList.contains('hidden')) {
         dark.classList.remove('hidden');
         light.classList.add('hidden');
       }
   } else {
-    html.classList.remove('dark');
+    htmlUpdate.classList.remove('dark');
     }
 }
 
 // ADD TOOLTIPS TO SKILL ICONS ON HOVER
-const skills = document.querySelectorAll('#skill-item img');
+const skills = document.querySelectorAll('.skill-item img');
 
 skills.forEach((skill) => {
   const skillSibling = skill.nextElementSibling;
@@ -100,6 +111,8 @@ window.onload = function () {
   });
 }
 
+let projectData;
+
 // PORTFOLIO GRID GENERATION BASED ON JSON OBJECTS
 window.onload = function () {
   // Fetch the JSON file
@@ -109,28 +122,27 @@ window.onload = function () {
       const projectCount = data.projects.length;
       // Iterate through all objects in JSON
       for(i = 0; i < projectCount; i++) {
-        let projectData = data.projects[i];
-        // let zData = data.projects[i];
+        projectData = data.projects[i];
         renderCards(projectData);
-        // openPortfolio(zData);
       }
     })
     .catch(error => {
       console.error(error);
     });
+    // openPortfolio(projectData);
 }
 
 function renderCards(dataRef) {
   // Render card on page and set ID's
   const portfolioSection = document.getElementById('portfolio');
   const newProject = document.createElement('div');
-  newProject.setAttribute('id', 'card-item');
-  newProject.setAttribute('class', 'card max-w-[31%]');
+  newProject.setAttribute('id', 'card-item-' + dataRef.id);
+  newProject.setAttribute('class', 'card md:max-w-[48%] lg:max-w-[31%]');
   portfolioSection.appendChild(newProject);
 
   // Main image
   const mainImage = document.createElement('div');
-  mainImage.setAttribute('id', 'main-image');
+  mainImage.setAttribute('id', 'main-image-' + dataRef.id);
   mainImage.setAttribute('class', 'flex w-full');
   newProject.appendChild(mainImage);
   // IMG
@@ -141,21 +153,21 @@ function renderCards(dataRef) {
 
   // Container 1 DIV
   const container1 = document.createElement('div');
-  container1.setAttribute('id', 'container-1');
+  container1.setAttribute('id', 'container-1-' + dataRef.id);
   container1.setAttribute('class', 'flex flex-row w-full justify-center content-center px-2 pt-4 pb-3');
   newProject.appendChild(container1);
 
   // Project name
-  const projectNameList = "flex flex-wrap justify-start content-center w-6/12 text-slate-600 font-bold text-lg";
+  const projectNameList = "flex flex-wrap justify-start content-center w-6/12 text-slate-600 font-bold text-md sm:text-lg dark:text-slate-50";
   const projectName = document.createElement('div');
-  projectName.setAttribute('id', 'project-name');
+  projectName.setAttribute('id', 'project-name-' + dataRef.id);
   projectName.setAttribute('class', projectNameList);
   projectName.textContent = dataRef.projectName;
   container1.appendChild(projectName);
 
   // Container 2 DIV
   const container2 = document.createElement('div');
-  container2.setAttribute('id', 'container-2');
+  container2.setAttribute('id', 'container-2-' + dataRef.id);
   container2.setAttribute('class', 'flex justify-end w-6/12');
   container1.appendChild(container2);
 
@@ -201,7 +213,6 @@ function renderCards(dataRef) {
       techUsed.appendChild(techUsedLi);
       // Create IMG 
       const techImage = document.createElement('img');
-      techImage.setAttribute('id', 'tech-used');
       techImage.setAttribute('src', '');
       techImage.setAttribute('src', techObject[key].src);
       techImage.setAttribute('alt', techObject[key].data);
@@ -211,19 +222,20 @@ function renderCards(dataRef) {
   
   // Excerpt
   const excerpt = document.createElement('div');
-  excerpt.setAttribute('id', 'excerpt');
+  excerpt.setAttribute('id', 'excerpt-' + dataRef.id);
   excerpt.setAttribute('class', 'flex flex-row w-full justify-start content-center px-2 pt-2');
   excerpt.textContent = dataRef.excerpt;
   newProject.appendChild(excerpt);
   
   // Button Div
   const viewButtonBox = document.createElement('div');
-  viewButtonBox.setAttribute('id', 'view-button');
+  viewButtonBox.setAttribute('id', 'view-button-box-' + dataRef.id);
   viewButtonBox.setAttribute('class', 'flex flex-row w-full justify-center content-center px-2 pt-6');
   newProject.appendChild(viewButtonBox);
   // Button
   let viewButton = document.createElement('a');
-  viewButton.setAttribute('id', 'view-button-btn');
+  // const rand = Math.floor(Math.random() * 100);
+  viewButton.setAttribute('id', 'view-button-' + dataRef.id);
   viewButton.setAttribute('class', 'w-full bg-sky-900 hover:bg-sky-800 transition ease-in-out duration-500 p-3 text-slate-50 text-center rounded-md text-[1.05rem] font-semibold cursor-pointer');
   // viewButton.setAttribute('href', dataRef.link);
   // viewButton.setAttribute('target', '_blank');
@@ -240,60 +252,37 @@ function renderCards(dataRef) {
   }
 }
 
+console.log(projectData); // why is this undefined? It's in the global scope?
+
 // OPEN PROJECT IN WINDOW ON BUTTON CLICK
 // Add event listener for button click
+setTimeout(() => {
+  const viewPortfolioItem = document.getElementById('view-button-5');
+  viewPortfolioItem.addEventListener('click', openPortfolio); // why is this event not functioning?
+}, 1000);
 
-
-// setTimeout(() => {
-//   const myVar = document.getElementById('view-button-btn');
-//   myVar.addEventListener('click', func);
-// }, 1000);
-
-// function func() {
-//   console.log('hey');
-// }
-
-document.body.addEventListener('load', function () {
-  const myVar = document.getElementById('view-button-btn');
-  console.log(myVar);
-  // myVar.addEventListener('click', func);
-
-  // function func() {
-  //   console.log('hey');
+// When clicked, change p-window to visible & render data
+function openPortfolio() {
+  let pWindow = document.getElementById('p-window');
+  function windowClose() {
+    pWindow.classList.toggle('hidden');
+    console.log('I was clicked');
+  }
+  // Close button
+  const closeWindow = document.querySelector('.close');
+  closeWindow.addEventListener('click', windowClose);
+  // Project name
+  // const windowProjectName = document.createElement('div');
+  // windowProjectName.setAttribute('class', 'text-slate-700');
+  // windowProjectName.textContent = projectDataRef.id;
+  // pWindow.appendChild(windowProjectName);
+  // console.log(projectDataRef[i]);
+  // if (projectData.id === 5) {
+  //   console.log('5', projectData);
+  // } else {
+  //   console.log('not 5');
   // }
-});
-
-
-// const myVar = document.querySelector('#about-image');
-// myVar.addEventListener('click', func);
-
-// function func() {
-//   console.log('hey');
-// }
-
-
-
-// function openPortfolio() {
-//   console.log('hey');
-// }
-// // When clicked, change p-window to visible & render data
-// function openPortfolio() {
-//   let pWindow = document.getElementById('p-window');
-//   function windowClose() {
-//     pWindow.classList.toggle('hidden');
-//   }
-//   windowClose();
-//   // Close button
-//   const closeWindow = document.createElement('a');
-//   closeWindow.setAttribute('class', 'flex absolute top-3 right-4 cursor-pointer font-bold text-sm text-red-400');
-//   closeWindow.textContent = "CLOSE";
-//   pWindow.appendChild(closeWindow);
-//   closeWindow.addEventListener('click', windowClose);
-//   // Project name
-//   const windowProjectName = document.createElement('div');
-//   windowProjectName.setAttribute('class', 'text-slate-700');
-//   pWindow.appendChild(windowProjectName);
-// }
+}
 
 
 
@@ -302,6 +291,3 @@ document.getElementById('year').innerText = "\u00A0" + new Date().getFullYear() 
 
 // LOAD CONTENT FROM README.MD & SHOW ON CHANGELOG PAGE
 // Need to setup Webpack first, then use remark-loader (https://www.npmjs.com/package/remark-loader?activeTab=readme)
-// import md from "README.md";
-
-// console.log(md);
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,13 @@ module.exports = {
     fontFamily: {
       'sans': ['Pacaembu, sans-serif'],
       'body': ['Mulish, sans-serif'],
-    }
+    },
+    screens: {
+      'sm': '640px',
+      'md': '1024px',
+      'lg': '1200px',
+      'xl': '1440px',
+    },
   },
   plugins: [],
 }
-
